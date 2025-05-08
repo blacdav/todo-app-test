ffe4f505981fd09009dfb577fe67024a03080758
Blacdav <davidaniefoik@gmai.com>
2025-05-08 06:04:56 +0100

    finished all the UI

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,13 +5,13 @@ import React from 'react'
 const Page = () => {
   return (
     <main>
-      <section className='fixed'>
-        <Image src='/assets/shape.svg' alt="shape" width={200} height={200} />
-      </section>
+      <section className='relative'>
+        <Image src='/assets/shape.svg' alt="shape" width={200} height={200} className='absolute top-0 left-0' />
 
-      <section className='bg-primary flex flex-col justify-end gap-3 pb-5 h-64 items-center'>
-        <Image src='/assets/Ellipse.png' alt='ellipse' width={100} height={100} />
-        <p className='font-bold text-secondary'>Welcome Jeegar goyani</p>
+        <section className='bg-primary flex flex-col justify-end gap-3 pb-5 h-64 items-center'>
+            <Image src='/assets/Ellipse.png' alt='ellipse' width={100} height={100} />
+            <p className='font-bold text-secondary'>Welcome Jeegar goyani</p>
+        </section>
       </section>
 
       <section className='flex flex-col pt-3 pe-5 gap-2'>
@@ -19,7 +19,7 @@ const Page = () => {
         <Image src='/assets/clock.png' alt="shape" width={200} height={200} className='self-center' />
       </section>
 
-      <section className='px-5'>
+      <section className='px-5 mb-5'>
         <p className='font-bold mb-5'>Task List</p>
         <div className='p-5 shadow-lg shadow-black/30 min-h-80 rounded-2xl'>
             <div className='flex justify-between items-center mb-12'>
